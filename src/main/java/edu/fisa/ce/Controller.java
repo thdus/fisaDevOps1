088528e0d2a3f622a0fbe6c82bcb3ab102e79f4a
sy <sykim0659@naver.com>
2026-03-24 16:46:56 +0900

    init

---
+++ src/main/java/edu/fisa/ce/Controller.java
@@ -0,0 +1,17 @@
+package edu.fisa.ce;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+	@GetMapping("ce3")
+	public String getData() {
+		System.out.println("요청 및 응답 --------------");
+		return "data";
+		
+		
+	}
+
+}
