d433276be4e865d5ea0471a2a173b21515c96ab8
sy <sykim0659@naver.com>
2026-03-24 16:54:12 +0900

    3

--- src/main/java/edu/fisa/ce/Controller.java
+++ src/main/java/edu/fisa/ce/Controller.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Controller {
 
-	@GetMapping("ce4")
+	@GetMapping("ce3")
 	public String getData() {
 		System.out.println("요청 및 응답 --------------");
 		return "data";
